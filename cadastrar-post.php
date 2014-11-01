f51fcedcf577e3a3c6ad146d6058250a1258e250
Andre Cardoso <andrecardosodev@gmail.com>
2014-10-31 22:56:24 -0200

    Criado formulário para cadastro do post

--- cadastrar-post.php
+++ cadastrar-post.php
@@ -1,6 +1,21 @@
 <?php include './templates/cabecalho.php'; ?>
 <?php include './templates/menu.php'; ?>
 
-    Aqui será cadastrado um novo post
+Informe os dados para o novo post
+<br /><br />
+
+<form method="post">
+    <label>Título</label>
+    <br />
+    <input type="text" name="titulo">
+    <br /><br />
+    <label>Conteúdo</label>
+    <br />
+    <textarea name="conteudo"  cols="50" rows="10"></textarea>
+    <br /><br />
+    
+    <input type="submit" value="Cadastrar">
+    
+</form>
 
 <?php include 'templates/rodape.php'; ?>
