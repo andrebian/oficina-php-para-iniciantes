b00b0b09145ac48122ac776b58b5311ea238a997
Andre Cardoso <andrecardosodev@gmail.com>
2014-10-31 22:15:40 -0200

    Adicionados links para a página inicial e para o cadastro de posts

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
     <head>
         <title>PHP na prática - Blog</title>
     </head>
-    
+
     <body>
-        Aqui será disposto o conteúdo do blog
+        <center>
+            <h1>PHP na prática</h1>
+            <h2>Construindo um blog</h2>
+            <a href="/blog">Página inicial</a> | 
+            <a href="cadastrar-post.php">Cadastrar post</a>
+        </center>
     </body>
 </html>
