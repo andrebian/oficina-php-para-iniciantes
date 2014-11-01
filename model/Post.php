6840b2eeb2e6269392c257d8eb356cda6b1c9a31
Andre Cardoso <andrecardosodev@gmail.com>
2014-10-31 23:23:23 -0200

    Implementado método salva

--- model/Post.php
+++ model/Post.php
@@ -6,6 +6,24 @@ class Post
 {
     public function salva($titulo, $conteudo, $id = null)
     {
+        abreConexao();
+        
+        $criado = date('Y-m-d H:i:s');
+        $atualizado = $criado;
+        
+        $sql = "INSERT INTO posts (titulo, conteudo, criado, atualizado) VALUES ";
+        $sql .= "('{$titulo}', '{$conteudo}', '{$criado}', '{$atualizado}')";
+        
+        if( !empty($id) && is_int($id) ) {
+            $sql = "UPDATE posts SET titulo='{$titulo}', conteudo='{$conteudo}', ";
+            $sql .= "atualizado='{$atualizado}' WHERE id={$id}";
+        }
+        
+        mysql_query($sql);
+        if( mysql_affected_rows() && !mysql_error() ) {
+            return true;
+        }
+        
         return false;
     }
 }
