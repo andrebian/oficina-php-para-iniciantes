8851819877e7777885209466272527026053965a
Andre Cardoso <andrecardosodev@gmail.com>
2014-10-31 23:09:28 -0200

    Criadas funções para abrir e fechar conexão

--- config/conexao-db.php
+++ config/conexao-db.php
@@ -1,4 +1,12 @@
 <?php
 
-mysql_connect('localhost', 'root', 'root') or die(mysql_error());
-mysql_select_db('blog') or die(mysql_error());
+function abreConexao()
+{
+    mysql_connect('localhost', 'root', 'root') or die(mysql_error());
+    mysql_select_db('blog') or die(mysql_error());
+}
+
+function fechaConexao() 
+{
+    mysql_close();
+}
